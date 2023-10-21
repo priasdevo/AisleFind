013dc66939715b4d588eed8e20357f96ace1c9a0
Pisuttun <pisuttun@gmail.com>
2023-10-21 16:32:18 +0700

    feat: new button in header

--- web/src/component/header.tsx
+++ web/src/component/header.tsx
@@ -1,12 +1,20 @@
 import './header.scss'
 import { AiOutlineSearch } from 'react-icons/ai'
+import { MdLogout } from 'react-icons/md'
+import { Typography } from "@mui/material";
 
 export default function Header(){
   return (
     <div className="header-container">
       <h2>AisleFind</h2>
-      <button className="search"><AiOutlineSearch/></button>
-      <button className="search"><AiOutlineSearch/></button>
+      <button className="small-button"><AiOutlineSearch/></button>
+
+      <button className="button">
+        <p>
+          รายชื่อร้านค้า
+        </p>
+      </button>
+      <button className="small-button"><MdLogout/></button>
     </div>
   )
 }
