810e32ff2d452164d0a0ecd3478352e86f351c3a
SuitedIceZ <okshopm575@gmail.com>
2023-10-24 21:23:05 +0700

    feat: convert REST API to gRPC

--- api/src/route/serviceRouter.ts
+++ api/src/route/serviceRouter.ts
@@ -1,24 +1,28 @@
 import express from 'express';
-import { handleRESTServiceRequest } from '../controllers/RESTService'
+import { handleRESTServiceRequest } from '../controllers/RESTController' 
+
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+import storeRouter from './storeRouter'; 
+
 
 const router = express.Router();
 
+// Get URL path from .env
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 const userService_URL = process.env.USER_SERVICE_URL || 'http://localhost:5000';
-const aisleService_URL = process.env.AISLE_SERVICE_URL || 'http://localhost:5010';
-const storeService_URL = process.env.STORE_SERVICE_URL || 'http://localhost:5020';
-const dataVisualizerService_URL = process.env.DATA_VIS_SERVICE_URL || 'http://localhost:5030';
+const aisleService_URL = process.env.AISLE_SERVICE_URL || 'http://localhost:7000';
+const storeService_URL = process.env.STORE_SERVICE_URL || 'http://localhost:50051';
+const dataVisualizerService_URL = process.env.DATA_VIS_SERVICE_URL || 'http://localhost:50052';
 
 // Route to User Service
 router.use('/user', handleRESTServiceRequest(userService_URL));
 // Route to Aisle Service
 router.use('/aisle', handleRESTServiceRequest(aisleService_URL));
 // Route to Store Service // TODO: change to gRPC maybe?
-router.use('/store', handleRESTServiceRequest(storeService_URL));
-// Route to Data Visualizer Service // TODO: change to gRPC maybe?
-router.use('/dataviz', handleRESTServiceRequest(dataVisualizerService_URL));
+router.use('/store', storeRouter);
+// // Route to Data Visualizer Service // TODO: change to gRPC maybe?
+// router.use('/dataviz', handleRESTServiceRequest(dataVisualizerService_URL));
 
 export default router;
---
+++ api/src/route/storeRouter.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { 
+    handleGetStoresList, 
+    handleCreateStore, 
+    handleUpdateStore, 
+    handleDeleteStore, 
+    handleGetStore,
+    handleGetStoreLayout,
+    handleAddLayout,
+    handleUpdateLayout,
+    handleDeleteLayout
+} from '../controllers/storeController';
+
+const storeRouter = express.Router();
+
+// Get the list of stores
+storeRouter.get('/', handleGetStoresList());
+// Add a new store
+storeRouter.post('/', handleCreateStore());
+// Update a store
+storeRouter.put('/:id', handleUpdateStore());
+// Delete a store
+storeRouter.delete('/:id', handleDeleteStore());
+
+// layout
+
+// Get a specific store
+storeRouter.get('/:id', handleGetStore());
+// Get a store layout
+storeRouter.get('/:id/layout', handleGetStoreLayout());
+// Add a new layout to a store
+storeRouter.post('/:id/layout', handleAddLayout());
+// Update a layout in a store
+storeRouter.put('/:id/layout/:layoutId', handleUpdateLayout());
+// Delete a layout in a store
+storeRouter.delete('/:id/layout/:layoutId', handleDeleteLayout());
+
+export default storeRouter;
