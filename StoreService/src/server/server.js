71d2922cbc2d4ce6304c7131b43390d20ae13b6f
SuitedIceZ <okshopm575@gmail.com>
2023-11-06 15:22:49 +0700

    fix: REST forward query string and open public bind for StoreService

--- StoreService/src/server/server.js
+++ StoreService/src/server/server.js
@@ -40,7 +40,7 @@ server.addService(storeProto.StoreService.service, {
 })
 
 server.bindAsync(
-  "127.0.0.1:50051",
+  "0.0.0.0:50051",
   grpc.ServerCredentials.createInsecure(),
   (error, port) => {
     console.log("Server running at http://127.0.0.1:50051");
