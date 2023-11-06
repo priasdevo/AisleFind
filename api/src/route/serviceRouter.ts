806114b65435faab8854614e1b5fb6562eb38fc5
Pisuttun <pisuttun@gmail.com>
2023-11-06 21:43:08 +0700

    feat: move to consul

--- api/src/route/serviceRouter.ts
+++ api/src/route/serviceRouter.ts
@@ -3,26 +3,54 @@ import { handleRESTServiceRequest } from '../controllers/RESTController'
 
 import dotenv from "dotenv";
 import path from "path";
+import Consul from "consul";
 
-import storeRouter from './storeRouter'; 
+import storeRouter from './storeRouter';
 
 
 const router = express.Router();
-
-// Get URL path from .env
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
-const userService_URL = process.env.USER_SERVICE_URL || 'http://localhost:5000';
-const aisleService_URL = process.env.AISLE_SERVICE_URL || 'http://localhost:7000';
-const storeService_URL = process.env.STORE_SERVICE_URL || 'http://localhost:50051';
-const dataVisualizerService_URL = process.env.DATA_VIS_SERVICE_URL || 'http://localhost:50052';
 
-// Route to User Service
-router.use('/user', handleRESTServiceRequest(userService_URL));
-// Route to Aisle Service
-router.use('/aisle', handleRESTServiceRequest(aisleService_URL));
-// Route to Store Service // TODO: change to gRPC maybe?
-router.use('/store', storeRouter);
-// // Route to Data Visualizer Service // TODO: change to gRPC maybe?
-// router.use('/dataviz', handleRESTServiceRequest(dataVisualizerService_URL));
+process.setMaxListeners(0);
+
+const consulClient = new Consul({
+  host: process.env.CONSUL_HOST || "localhost",
+  port: process.env.CONSUL_PORT || "8500",
+});
+
+interface consulNode {
+  ServiceAddress: string;
+  ServicePort: number;
+}
+async function getURL(serviceName:string){
+  try{
+    const services:consulNode[] = await consulClient.catalog.service.nodes(serviceName);
+    return services[0].ServiceAddress + ":" + services[0].ServicePort
+  }
+  catch(err){
+    console.log(err)
+    return ''
+  }
+}
+
+// Get URL path from consul
+let userService_URL = 'http://localhost:5000';
+let aisleService_URL = 'http://localhost:7000';
+let storeService_URL = 'http://localhost:50051';
+
+async function setURLs(){
+  userService_URL = await getURL('User-service')
+  aisleService_URL = await getURL('Aisle-service')
+  storeService_URL = await getURL('Store-service')
+  console.log(userService_URL)
+  // Route to User Service
+  router.use('/user', handleRESTServiceRequest(userService_URL));
+  // Route to Aisle Service
+  router.use('/aisle', handleRESTServiceRequest(aisleService_URL));
+  // Route to Store Service // TODO: change to gRPC maybe?
+  router.use('/store', storeRouter);
+}
+setURLs()
+
 
 export default router;
